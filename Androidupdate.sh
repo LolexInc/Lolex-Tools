561f4a189525b2d5c0f26dfdc2ddb8f1d8ec4045
Monkeyboy2805 <monkeyboy2805@gmail.com>
2016-11-28 18:19:01 +0000

    armv7 auto copy android. Update test for Android

---
+++ Androidupdate.sh
@@ -0,0 +1,3 @@
+cd /sdcard/Lolex-Tools
+git pull
+python3 /sdcard/Lolex-Tools/Androidfirsttimeinit.py
--- start.py
+++ start.py
@@ -1,5 +1,11 @@
 import subprocess,os, platform
 system = platform.system()
+if "armv7" in platform.platform():
+	try:
+		import androidlolextoolsinit
+	except(ImportError):
+		print("If ALL files are not present in /sdcard/Lolex-Tools this will fail...")
+		os.system("python3 /sdcard/Lolex-Tools/Androidfirsttimeinit.py")
 	
 if system == "Windows":
 	subprocess.call("./JTTools.py",shell = True)
