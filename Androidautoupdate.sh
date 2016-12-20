b7043b9b3079bc4d09814576bc2fc6b01161ab46
Monkeyboy2805 <lolexinc2805@gmail.com>
2016-12-20 18:13:38 +0000

    Remove some non-necessities and change pull to rebase
    That way if you're developing you can merge your changes with the latest public changes.

--- Androidautoupdate.sh
+++ Androidautoupdate.sh
@@ -1,7 +1,5 @@
 echo "Please be aware that this will NOT work if all files are not present in /sdcard/Lolex-Tools"
 cd /sdcard/Lolex-Tools
-echo "Installing/updating git..."
-apt install git
-git pull
+git pull --rebase
 echo "Please restart your terminal or type cd <PATH> to go back to the default path. Type your path in place of <PATH>."
 exit
