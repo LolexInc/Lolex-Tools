543cf679d8053cd6bc94c2d320d5b6365453bcf9
Monkeyboy2805 <lolexinc2805@gmail.com>
2017-01-24 13:55:34 +0000

    Add base code for more hidden modes

--- LolexTools.py
+++ LolexTools.py
@@ -252,32 +252,48 @@ try:
                 print("1 = Hide power menu modes.")
                 settinga = int(input("Please input the number of the setting you wish to modify."))
                 if settinga == 1:
-                    print(modetwo, "hidden = ", restartsettings.hidden)
-                    print(modethree, "hidden = ", logoffsettings.hidden)
-                    print(modefourlinux, "hidden = ", hibernatesettings.hidden)
-                    print(modefivelinux, "hidden = ", shutdownsettings.hidden)
+                    print("2 = Restart hidden = ", restartsettings.hidden)
+                    print("3 = Logoff hidden = ", logoffsettings.hidden)
+                    print("4 = Hibernate hidden = ", hibernatesettings.hidden)
+                    print("5 = Shutdown hidden = ", shutdownsettings.hidden)
+                    print("6 = Call a Python Shell hidden = ", pyshellsettings.hidden)
+                    print("7 = Create folders hidden = ", foldercreatesettings.hidden)
+                    print("8 = Remove folders hidden = ", exfoldersettings.hidden)
+                    print("9 = Create files hidden = ", addfilesettings.hidden)
+                    print("10 = Restart this script hidden = ", scriptloopsettings.hidden)
+                    print("11 = Perform arithmetic operations hidden = ", mathmodesettings.hidden)
+                    print("12 = Lock this script hidden = ", scriptlocksettings.hidden)
                     hidestate = int(input("Please select the number of the mode."))
                     if hidestate == 2:
                         LolexToolsMethods.modehide("restart", restartsettings.hidden)
-                        restartneeded = True
                     elif hidestate == 3:
                         LolexToolsMethods.modehide("logoff", logoffsettings.hidden)
-                        restartneeded = True
                     elif hidestate == 4:
                         LolexToolsMethods.modehide("hibernate", hibernatesettings.hidden)
-                        restartneeded = True
                     elif hidestate == 5:
                         LolexToolsMethods.modehide("shutdown", shutdownsettings.hidden)
-                        restartneeded = True
-                    if restartneeded == True:
-                        if useros == "Windows":
-                            if sys.version_info.minor>5:
-                                os.system("py .\start.py")
-                            else:
-                                os.system("python .\start.py")
+                    elif hidestate == 6:
+                        LolexToolsMethods.modehide("pyshell", pyshellsettings.hidden)
+                    elif hidestate == 7:
+                        LolexToolsMethods.modehide("foldercreate", foldercreatesettings.hidden)
+                    elif hidestate == 8:
+                        LolexToolsMethods.modehide("exfolder", exfoldersettings.hidden)
+                    elif hidestate == 9:
+                        LolexToolsMethods.modehide("addfile", addfilesettings.hidden)
+                    elif hidestate == 10:
+                        LolexToolsMethods.modehide("scriptloop", scriptloopsettings.hidden)
+                    elif hidestate == 11:
+                        LolexToolsMethods.modehide("mathmode", mathmodesettings.hidden)
+                    elif hidestate == 12:
+                        LolexToolsMethods.modehide("scriptlock", scriptlocksettings.hidden)
+                    if useros == "Windows":
+                        if sys.version_info.minor>5:
+                            os.system("py .\start.py")
                         else:
-                            os.system("python3 ./start.py")
-                        exit(None)
+                            os.system("python .\start.py")
+                    else:
+                        os.system("python3 ./start.py")
+                    exit(None)
         elif modewanted == 2:
             LolexToolsMethods.mode2()
         elif modewanted == 3:
