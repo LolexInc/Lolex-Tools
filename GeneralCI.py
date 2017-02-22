be4ea83b5055419a4c3bf5f2391c417db629a4d3
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-02-22 17:48:28 +0000

    Start CI.py

---
+++ GeneralCI.py
@@ -0,0 +1,7 @@
+import py_compile
+try:
+    py_compile.compile("./LolexTools.py")
+    py_compile.compile("./LolexToolsMethods.py")
+    py_compile.compile("./LolexToolsInstaller.py")
+except(SyntaxError):
+    print("Build failed!")
