dba443ceaf3f9ab4a6adfb7512421f19b066e219
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-02-24 18:24:48 +0000

    Start Concept

--- CICompile.py
+++ CICompile.py
@@ -1,1 +1,14 @@
-
+import os, py_compile
+class vars:
+    path = "./"
+    root = os.listdirs("./")
+    folders = []
+    files = []
+    arraypos = 0
+    arraylength = len(root)
+while vars.arraypos < vars.arraylength:
+    try:
+        os.listdirs(vars.root[vars.arraypos])
+        vars.folders.append("./" + vars.root[vars.arraypos])
+    except(IOError, OSError):
+        pass
