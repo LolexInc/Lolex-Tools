29cad3a1862705f19a19615f73fbe62cc39521ce
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-02-24 16:02:34 +0000

    Update 9.0nann1.py

--- update/9.0nann1.py
+++ update/9.0nann1.py
@@ -8,19 +8,20 @@ try:
 except(IOError, OSError):
     pass
 with open ("./verifonboot.py","a") as outf:
-    if LolexToolsOptions.onepintotal>0:
+    if LolexToolsOptions.onepintotal>1:
         outf.write("oneswappins = True\nruntimeone = ")
         outf.write(str(verifonboot.runtimeone))
-    if LolexToolsOptions.onewordtotal>0:
+    if LolexToolsOptions.onewordtotal>1:
         outf.write("oneswapwords = True\nwordtimeone = ")
         outf.write(str(verifonboot.wordtimeone))
-    if LolexToolsOptions.twopintotal>0:
+    if LolexToolsOptions.twopintotal>1:
         outf.write("twoswappins = True\nruntimetwo = ")
         outf.write(str(verifonboot.runtimetwo))
-    if LolexToolsOptions.twowordtotal>0:
+    if LolexToolsOptions.twowordtotal>1:
         outf.write("twoswappins = True\nwordtimetwo = ")
         outf.write(str(verifonboot.wordtimetwo))
 if LolexToolsOptions.compiler == True:
     LolexToolsMethods.compiler("verifonboot")
-print("Finished updating to 9.0nann1...")
+print("Finished updating to 9.0nann2...")
+with open("./madeon.py", "a") as outf: outf.write("compiledon = 9.00001")
         
