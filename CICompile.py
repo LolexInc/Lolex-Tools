797a463b0ee918429504d8874c41205a56f3f990
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-02-25 18:26:38 +0000

    Update CICompile.py

--- CICompile.py
+++ CICompile.py
@@ -11,4 +11,28 @@ while vars.arraypos < vars.arraylength:
         os.listdirs(vars.root[vars.arraypos])
         vars.folders.append("./" + vars.root[vars.arraypos])
     except(IOError, OSError):
-        pass
+        vars.files.append("./" + vars.root[vars.arraypos])
+    vars.arraypos = vars.arraypos + 1
+vars.arraypos = 0
+vars.arraylength = len(vars.folders)
+while vars.arraypos<vars.arraylength:
+    vars.path = "./" + vars.folders[vars.arraypos]
+    class number2:
+        currsub = os.listdirs(vars.path)
+        arraypos = 0
+        arraylength = len(currsub)
+    while number2.arraypos<number2.arraylength:
+        try:
+            os.listdirs(number2.currsub[number2.arraypos])
+            vars.folders.append(vars.path + number2.currsub[number2.arraypos])
+            number2.arraypos = number2.arraypos + 1
+        except(IOError, OSError):
+            vars.files.append(vars.path + number.currsub[number2.arraypos])
+        number2.arraypos = number2.arraypos + 1
+filearraylen = len(vars.files)
+vars.arraypos = 0
+while vars.arraypos<filearraylen:
+    indivlen = len(vars.files[vars.arraypos])
+    if vars.files[vars.arraypos[indiveln - 2]] == "y" and vars.files[vars.arraypos[indiveln - 3]] == "p" and vars.files[vars.arraypos[indiveln - 4]] == ".":
+        py_compile.compile(vars.files[vars.arraypos])
+        vars.arraypos = vars.arraypos + 1
