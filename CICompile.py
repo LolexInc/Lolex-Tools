9fa97f75157c1ca4d1ab78174e828f13455c6bfc
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-02-26 18:19:13 +0000

    Fix wrong method name

--- CICompile.py
+++ CICompile.py
@@ -1,14 +1,14 @@
 import os, py_compile
 class vars:
     path = "./"
-    root = os.listdirs("./")
+    root = os.listdir("./")
     folders = []
     files = []
     arraypos = 0
     arraylength = len(root)
 while vars.arraypos < vars.arraylength:
     try:
-        os.listdirs(vars.root[vars.arraypos])
+        os.listdir(vars.root[vars.arraypos])
         vars.folders.append("./" + vars.root[vars.arraypos])
     except(IOError, OSError):
         vars.files.append("./" + vars.root[vars.arraypos])
@@ -18,12 +18,12 @@ vars.arraylength = len(vars.folders)
 while vars.arraypos<vars.arraylength:
     vars.path = "./" + vars.folders[vars.arraypos]
     class number2:
-        currsub = os.listdirs(vars.path)
+        currsub = os.listdir(vars.path)
         arraypos = 0
         arraylength = len(currsub)
     while number2.arraypos<number2.arraylength:
         try:
-            os.listdirs(number2.currsub[number2.arraypos])
+            os.listdir(number2.currsub[number2.arraypos])
             vars.folders.append(vars.path + number2.currsub[number2.arraypos])
             number2.arraypos = number2.arraypos + 1
         except(IOError, OSError):
@@ -35,4 +35,5 @@ while vars.arraypos<filearraylen:
     indivlen = len(vars.files[vars.arraypos])
     if vars.files[vars.arraypos[indiveln - 2]] == "y" and vars.files[vars.arraypos[indiveln - 3]] == "p" and vars.files[vars.arraypos[indiveln - 4]] == ".":
         py_compile.compile(vars.files[vars.arraypos])
+        print("Successfully compiled " + vars.files[vars.arraypos])
         vars.arraypos = vars.arraypos + 1
