7c77b84821beba1f5e1d4b0b4229d0e838dff010
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-03-01 16:10:24 +0000

    Finish new version

--- CI.py
+++ CI.py
@@ -13,5 +13,36 @@ while arraypos < len(root):
             if p == "p" and y == "y" and dot == ".":
                 files.append("./" + root[arraypos])
             else:
-                folders.append("
+                folders.append("./" + root[arraypos])
+    arraypos = arraypos + 1
+arraypos = 0
+while arraypos<len(folders):
+    path = folders[arraypos]
+    currsub = os.listdir(path)
+    tarraypos = 0
+    sublen = len(currsub)
+    while tarrypos<sublen:
+        if (".git" in root[arraypos]) == False:
+            
+            if "." in root[arraypos]:
+                clen = len(currsub[tarraypos])
+                p = currsub[tarraypos][clen - 3]
+                y = currsub[tarraypos][clen - 2]
+                dot = currsub[tarraypos][clen - 4]
+                if p == "p" and y == "y" and dot == ".":
+                    files.append(path + currsub[tarraypos])
+                else:
+                    folders.append(path + currsub[tarraypos])
+          tarraypos = tarraypos + 1
+      arraypos = arraypos + 1
+flen = len(files)
+arraypos = 0
+while arraypos<flen:
+  currfile = file[arraypos]
+  py_compile.compile(currfile)
+  print("Successfully compiled " + currfile)
+  arraypos = arraypos + 1
+        
+                
+                
             
