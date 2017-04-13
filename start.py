b5f00ae8b39004d63c672b3fe205d3f02ba2b2b0
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-04-13 14:41:36 +0100

    Fix a bug

--- start.py
+++ start.py
@@ -26,87 +26,89 @@ if "arm" in platform.platform():
 	except(ImportError):
 		os.system("python3 /sdcard/Lolex-Tools/Androidfirsttimeinit.py")
 try:
-	try:
-		import JTToolsOptions
-		try:
-			os.rename("./JTToolsOptions.py","LolexToolsOptions.py")
-		except(IOError, OSError):
-			try:
-				os.rename("./JTToolsOptions.pyc","LolexToolsOptions.pyc")
-			except(IOError, OSError):
-				pass
-	except(ImportError):
-		pass
-	import LolexToolsOptions
-	if LolexToolsOptions.compiledon<8.127:
-		if platform.system() == "Windows":
-			if sys.version_info.minor>5:
-				os.system(" py .\LolexToolsInstaller.py")
-			else:
-				os.system("python .\LolexToolsInstaller.py")
-		else:
-			if "arm" in platform.system():
-				os.system("cd ./Lolex-Tools")
-			if platform.system() == "Linux":
-				os.system("python3 ./LolexToolsInstaller.py")
-	try:
-		import ver
-		if ver.version <= 8.129211346:
-			with open ("./exitsettings.py","a") as f: f.write("hidden = False")
-	except(IOError, ImportError):
-		pass
+        fail = False
+        try:
+                import JTToolsOptions
+                try:
+                        os.rename("./JTToolsOptions.py","LolexToolsOptions.py")
+                except(IOError, OSError):
+                        try:
+                                os.rename("./JTToolsOptions.pyc","LolexToolsOptions.pyc")
+                        except(IOError, OSError):
+                                pass
+        except(ImportError, SystemError):
+                fail = True
+        if fail != False:
+                import LolexToolsOptions
+                if LolexToolsOptions.compiledon<8.127:
+                        if platform.system() == "Windows":
+                                if sys.version_info.minor>5:
+                                        os.system(" py .\LolexToolsInstaller.py")
+                                else:
+                                        os.system("python .\LolexToolsInstaller.py")
+                        else:
+                                if "arm" in platform.system():
+                                        os.system("cd ./Lolex-Tools")
+                                if platform.system() == "Linux":
+                                        os.system("python3 ./LolexToolsInstaller.py")
+                try:
+                        import ver
+                        if ver.version <= 8.129211346:
+                                with open ("./exitsettings.py","a") as f: f.write("hidden = False")
+                except(IOError, ImportError):
+                        pass
 except(ImportError, IOError, OSError):
-	if platform.system() == "Windows":
-		if sys.version_info.minor>5:
-			os.system("py .\LolexToolsInstaller.py")
-		else:
-			os.system("python .\LolexToolsInstaller.py")
-	else:
-		os.system("python3 ./LolexToolsInstaller.py")
-	exit()
+        if platform.system() == "Windows":
+                if sys.version_info.minor>5:
+                        os.system("py .\LolexToolsInstaller.py")
+                else:
+                        os.system("python .\LolexToolsInstaller.py")
+        else:
+                os.system("python3 ./LolexToolsInstaller.py")
+
 if system == "Windows":
-	print("Starting on Windows...")
-	os.system("TITLE Lolex-Tools")
-	os.system("MODE 1000")
-	if sys.version_info.minor>5:
-		os.system("py .\sys/bootanim.py")
-		print("Starting...")
-		b = time.time()
-		local = time.asctime(time.localtime(time.time()))
-		with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
-			outf.write(local)
-			outf.write("	 Took ")
-			outf.write((str(b-a)))
-			outf.write("seconds to initilaize on Windows Python >3.5\n")
-		time.sleep(3)
-		os.system("py .\LolexTools.py")
-	else:
-		os.system("python .\sys/bootanim.py")
-		print("Starting...")
-		b = time.time()
-		local = time.asctime(time.localtime(time.time()))
-		with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
-			outf.write(local)
-			outf.write("	 Took ")
-			outf.write((str(b-a)))
-			outf.write("seconds to initilaize on Windows Python < 3.6.\n")
-		time.sleep(3)
-		os.system("python LolexTools.py")
-	exit(None)
+        print("Starting on Windows...")
+        os.system("TITLE Lolex-Tools")
+        os.system("MODE 1000")
+        if sys.version_info.minor>5:
+                os.system("py .\sys/bootanim.py")
+                print("Starting...")
+                b = time.time()
+                local = time.asctime(time.localtime(time.time()))
+                with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
+                        outf.write(local)
+                        outf.write("	 Took ")
+                        outf.write((str(b-a)))
+                        outf.write("seconds to initilaize on Windows Python >3.5\n")
+                time.sleep(3)
+                os.system("py .\LolexTools.py")
+        else:
+                os.system("python .\sys/bootanim.py")
+                print("Starting...")
+                b = time.time()
+                local = time.asctime(time.localtime(time.time()))
+                with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
+                        outf.write(local)
+                        outf.write("	 Took ")
+                        outf.write((str(b-a)))
+                        outf.write("seconds to initilaize on Windows Python < 3.6.\n")
+                time.sleep(3)
+                os.system("python LolexTools.py")
+        exit(None)
 else:
-		print("Starting on Linux...")
-		sys.stdout.write("\x1b]2;Lolex-Tools\x07")
-		os.system("sudo apt-get install xdotool")
-		os.system("xdotool key ctrl+super+Up")
-		os.system("python3 ./sys/bootanim.py")
-		print("Starting...")
-		b = time.time()
-		local = time.asctime(time.localtime(time.time()))
-		with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
-				outf.write(local)
-				outf.write("	 Took ")
-				outf.write((str(b-a)))
-				outf.write("seconds to initilaize on Linux Python 3.\n")
-		time.sleep(3)
-		os.system("python3 ./LolexTools.py")
-		exit(None)
+        print("Starting on Linux...")
+        sys.stdout.write("\x1b]2;Lolex-Tools\x07")
+        os.system("sudo apt-get install xdotool")
+        os.system("xdotool key ctrl+super+Up")
+        os.system("python3 ./sys/bootanim.py")
+        print("Starting...")
+        b = time.time()
+        local = time.asctime(time.localtime(time.time()))
+        with open ("./Logs/LolexToolsLogFile.txt","a") as outf:
+                outf.write(local)
+                outf.write("	 Took ")
+                outf.write((str(b-a)))
+                outf.write("seconds to initilaize on Linux Python 3.\n")
+        time.sleep(3)
+        os.system("python3 ./LolexTools.py")
+        exit(None)
