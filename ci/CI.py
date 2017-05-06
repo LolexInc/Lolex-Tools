5645adf171e2ee1e2797d82fc4a7b825bb94d6a6
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-05-06 10:53:47 +0100

    Reformat ci/ci.py

--- ci/CI.py
+++ ci/CI.py
@@ -15,52 +15,50 @@ root = os.listdir("./../")
 arraypos = 0
 filesl = 0
 while arraypos < len(root):
-    if (".git" in root[arraypos]) == False:
-        if "LICENSE" in root[arraypos]:
-        	pass
-        elif "." in root[arraypos]:
-            length = len(root[arraypos])
-            p = root[arraypos][length - 2]
-            y = root[arraypos][length - 1]
-            dot = root[arraypos][length - 3]
-            if p == "p" and y == "y" and dot == ".":
-                files.append("./../" + root[arraypos])
-        else:
-            folders.append("./../" + root[arraypos])
-    arraypos = arraypos + 1
+	if (".git" in root[arraypos]) == False:
+		if "." in root[arraypos]:
+			length = len(root[arraypos])
+			p = root[arraypos][length - 2]
+			y = root[arraypos][length - 1]
+			dot = root[arraypos][length - 3]
+			if p == "p" and y == "y" and dot == ".":
+				files.append("./../" + root[arraypos])
+		else:
+			folders.append("./../" + root[arraypos])
+	arraypos = arraypos + 1
 arraypos = 0
 print(folders)
 while arraypos<len(folders):
-    path = folders[arraypos] + "/"
-    currsub = os.listdir(path)
-    tarraypos = 0
-    sublen = len(currsub)
-    while tarraypos<sublen:
-        if (".git" in currsub[tarraypos]) == False:
-            if "." in currsub[tarraypos]:
-                clen = len(currsub[tarraypos])
-                p = currsub[tarraypos][clen - 2]
-                y = currsub[tarraypos][clen - 1]
-                dot = currsub[tarraypos][clen - 3]
-                if p == "p" and y == "y" and dot == ".":
-                    files.append(path + currsub[tarraypos])
-            else:
-                folders.append(path + currsub[tarraypos])
-        tarraypos = tarraypos + 1
-    arraypos = arraypos + 1
+	path = folders[arraypos] + "/"
+	currsub = os.listdir(path)
+	tarraypos = 0
+	sublen = len(currsub)
+	while tarraypos<sublen:
+		if (".git" in currsub[tarraypos]) == False:
+			if "." in currsub[tarraypos]:
+				clen = len(currsub[tarraypos])
+				p = currsub[tarraypos][clen - 2]
+				y = currsub[tarraypos][clen - 1]
+				dot = currsub[tarraypos][clen - 3]
+				if p == "p" and y == "y" and dot == ".":
+					files.append(path + currsub[tarraypos])
+			else:
+				folders.append(path + currsub[tarraypos])
+		tarraypos = tarraypos + 1
+	arraypos = arraypos + 1
 flen = len(files)
 arraypos = 0
 print("Compiling...")
 while arraypos<flen:
-    currfile = files[arraypos]
-    aarraypos = 0
-    py_compile.compile(currfile)
-    while aaraypos < len(currfile):
-    	if currfile[aarraypos] == "." and currfile[aarraypos  + 1] == "." and currfile[aarraypos + 2] == "/":
-            del currfile[aarraypos]
-            del currfile[aarraypos + 1]
-            del currfile[aarraypos + 2]
-        else:
-            aarraypos = aarraypos + 3
-    print("Successfully compiled " + (str(currfile)))
-    arraypos = arraypos + 1
+	currfile = files[arraypos]
+	aarraypos = 0
+	py_compile.compile(currfile)
+	while aarraypos < len(currfile):
+		if currfile[aarraypos] == "." and currfile[aarraypos  + 1] == "." and currfile[aarraypos + 2] == "/":
+			del currfile[aarraypos]
+			del currfile[aarraypos + 1]
+			del currfile[aarraypos + 2]
+		else:
+			aarraypos = aarraypos + 3
+			print("Successfully compiled " + (str(currfile)))
+		arraypos = arraypos + 1
