e635753bfd8e374be314af609f5bc83a3aa2ab1d
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-06-30 12:40:56 +0100

    Update 9.0alpha0.py

--- update/90/9.0alpha0.py
+++ update/90/9.0alpha0.py
@@ -1,2 +1,6 @@
+import os
 from lib import LolexToolsMethods
-LolexToolsMethods.addplugins(True)
+if os.path.exists("./update/90/9.0alpha0patch1.py") and (os.path.isdir("./update/90/9.0alpha0patch1.py")) == False:
+	pass
+else:
+	LolexToolsMethods.addplugins(True)
