8ddf9c2fc40877bf9a1f056b050d3543a94742a7
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:27:01 +0000

    Update LolexToolsMethods.py

--- lib/LolexToolsMethods.py
+++ lib/LolexToolsMethods.py
@@ -8,7 +8,7 @@
 ##
 ## authors = Monkeyboy2805
 import os, time, py_compile, shutil, sys, platform, threading, subprocess
-if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
+if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
         IOError = OSError
 print ("Module LolexToolsMethods is running, using modules os, time, py_compile, shutil, sys, platform, threading.")
 s = os.sep
