3c68a5f37b37c75aa20320517a404ea1c67b6e3d
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:50:20 +0000

    Add logo

--- update/90/9.0nann7.py
+++ update/90/9.0nann7.py
@@ -1,3 +1,12 @@
+#! python3
+##0
+## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+##  0              00     0  0         0           00             00       0      0    0      0   0          0
+##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
+##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+##
+## authors = Monkeyboy2805
 import os, py_compile, sys
 if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
 	IOError = OSError
