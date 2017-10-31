3fb6379c94f7b693e78e63fd32564387ba5fc275
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:33:57 +0000

    3.7alphaIOPatch

--- ci/CI.py
+++ ci/CI.py
@@ -8,7 +8,7 @@
 ##
 ## authors = Monkeyboy2805
 import os, py_compile, sys
-if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
+if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
 	IOError = OSError
 print("CI version 1.9.0")
 folders = []
