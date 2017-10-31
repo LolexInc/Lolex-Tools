b8fe07a881aba7c45f687f35f2f5ff5f455c7da1
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:37:25 +0000

    Add catch for 3.7alpha1

--- main/LolexTools.py
+++ main/LolexTools.py
@@ -8,6 +8,8 @@
 ##
 ## authors = Monkeyboy2805
 import sys, time, subprocess, os, shutil, py_compile, platform, zipfile, importlib
+if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
+	IOError = OSError
 try:
 	from lib import LolexToolsMethods
 except(ImportError, SyntaxError, TabError) as e:
