08d53336f50687a3fe9c457a217e8d30fd8e0416
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:48:03 +0000

    Update 9.0alpha0.py

--- update/90/9.0alpha0.py
+++ update/90/9.0alpha0.py
@@ -1,3 +1,12 @@
+#! python3
+##0
+## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+##  0              00     0  0         0           00             00       0      0    0      0   0          0
+##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
+##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+##
+## authors = Monkeyboy2805
 import os
 from lib import LolexToolsMethods
 if os.path.exists("./update/90/9.0alpha0patch1.py") and (os.path.isdir("./update/90/9.0alpha0patch1.py")) == False:
