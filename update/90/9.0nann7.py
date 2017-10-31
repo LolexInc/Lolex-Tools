d5d055bfcca4d988e1c2112e3f1f0c98192b5c38
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-10-31 15:51:17 +0000

    Remove some useless code

--- update/90/9.0nann7.py
+++ update/90/9.0nann7.py
@@ -10,7 +10,6 @@
 import os, py_compile, sys
 if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
 	IOError = OSError
-sys.path.append("./")
 import LolexToolsOptions
 array = os.listdir("./")
 if LolexToolsOptions.compiler == True:
