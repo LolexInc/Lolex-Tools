d6d9675f521b84a547c331ae0b6c9385c776152f
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-11-07 15:21:19 +0000

    patch2Part1

--- requiredpatches.py
+++ requiredpatches.py
@@ -1,1 +1,1 @@
-patches = "/90/9.0nann4", "/90/9.0nann7", "/90/9.0alpha0", "/90/9.0alpha0patch1"
+patches = "/90/9.0nann4", "/90/9.0nann7", "/90/9.0alpha0", "/90/9.0alpha0patch1", "/90/9.0alpha0patch2"
