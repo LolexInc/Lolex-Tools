e6e3fb3bac5bbd75a1836f944d05ecaa1c07eec8
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-11-29 15:33:29 +0000

    Fix some stuff with lib not being recognised with PyPortable 3.2.5.1

--- start.py
+++ start.py
@@ -14,8 +14,9 @@ if sys.version_info.major != 3:
 	exit(0)
 if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
 	IOError = OSError
+sys.path.append("./lib")
 try:
-	from lib import LolexToolsMethods
+	import LolexToolsMethods
 except(ImportError, SyntaxError, TabError) as e:
 	print(e)
 	print("Missing library. Please redownload this application.")
