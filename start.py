e702d8962fd06628a80f2881f92a4317a68c092c
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-11-30 09:56:17 +0000

    Rewrite start.py

--- start.py
+++ start.py
@@ -9,47 +9,27 @@
 ## authors = Monkeyboy2805
 import os, shutil, sys, time
 if sys.version_info.major != 3:
-	print("Please install Python 3 to run this script.")
-	time.sleep(5)
-	exit(0)
+    print("Please install Python 3 to run this script.")
+    time.sleep(5)
+    exit(0)
 if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
-	IOError = OSError
-sys.path.insert(0, "./lib")
+    IOError = OSError
+sys.path.insert(0, "./lib/")
 try:
-	import LolexToolsMethods
+    import LolexToolsMethods
 except(ImportError, SyntaxError, TabError) as e:
-	print(e)
-	print("Missing library. Please redownload this application.")
-	time.sleep(5)
-	exit(0)
-#if LolexToolsMethods.uos.useros == "Android":
-	#try:
-		#import androidinit
-		#try:
-			#os.remove("./ver_old.py")
-		#except(IOError, OSError):
-			#pass
-		#os.rename("./ver.py", "ver_old.py")
-		#shutil.copy("/sdcard/Lolex-Tools/ver.py", "./")
-		#import ver, ver_old
-		#if ver.version > ver_old.version:
-			#print("Installing updates...")
-			#os.system("python3 /sdcard/Lolex-Tools/Androidfirsttimeinit.py")
-	#except(ImportError, SyntaxError, TabError):
-		#os.system("python3 /sdcard/Lolex-Tools/Androidfirsttimeinit.py")
+    print(e)
+    print("Missing library. Please redownload this application.")
+    time.sleep(5)
+    exit(0)
 if LolexToolsMethods.uos.useros == "Windows":
-	os.system("TITLE Lolex-Tools")
-	os.system("MODE 1000")
-	os.system(LolexToolsMethods.pyo + " .\sys\\bootanim.py")
-	os.system(LolexToolsMethods.pyo + " ./main/LolexTools.py")
-	exit(0)
+    os.system("TITLE Lolex-Tools")
+    os.system("MODE 1000")
 elif LolexToolsMethods.uos.useros == "Linux" or LolexToolsMethods.uos.useros == "Android":
-	sys.stdout.write("\x1b]2;Lolex-Tools\x07")
-	if LolexToolsMethods.uos.useros == "Linux":
-		os.system("python3 ./sys/bootanim.py")
-	os.system("python3 ./main/LolexTools.py")
-	exit(0)
+    sys.stdout.write("\x1b]2;Lolex-Tools\x07")
 else:
-	print("OS not supported!!!")
-	time.sleep(5)
-	exit(0)
+    print("OS not supported!!!")
+    time.sleep(5)
+    exit(0)
+os.system(LolexToolsMethods.pyo + " ./sys/bootanim.py")
+os.system(LolexToolsMethods.pyo + " ./main/LolexTools.py")
