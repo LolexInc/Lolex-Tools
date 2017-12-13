e6233bcb445534dd04e05f39c9995002bd20e6d1
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-13 15:32:57 +0000

    Add preliminary user adding

--- setup/exp/LolexToolsSetup.py
+++ setup/exp/LolexToolsSetup.py
@@ -7,7 +7,27 @@
 ##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
 ##
 ## authors = Monkeyboy2805
-import time
+import time, os, shutil
+try:
+    shutil.rmtree("./setup/exp/TEST")
+except(IOError, OSError):
+    pass
+os.mkdir("./setup/exp/TEST")
+users = []
+users_file = open("./setup/ext/TEST/users.py", "w+")
+done = "0"
+while done != "1":
+    valid = True
+    user_name = input("Please enter your desired username.")
+    if len(users) > 0:
+        for i in range(0, len(users)):
+            if i == len(users):
+                break;
+            if users[i] == user_name:
+                print("Username is already taken!")
+            else:
+                users.append(user_name)
+    done = input("Please enter 1 if you are done adding users.")
 print("Welcome to the preliminary LolexToolsSetup. It doesn't actually do anything so we'll just exit now...")
 time.sleep(5)
 exit(0)
