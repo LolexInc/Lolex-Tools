2fa96508d3cd4d727dc62dfb3050df4e8ea05c2c
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-13 15:56:22 +0000

    Fix a bug

--- ext/exp_installer_run.py
+++ ext/exp_installer_run.py
@@ -1,5 +1,5 @@
 import os, sys
 if input("Do you want to enter the experimental installer? Please enter 1 if so.") == "1":
-    sys.path.insert(1, "./lib")
+    sys.path.insert(0, "./lib")
     import LolexToolsMethods
     os.system(LolexToolsMethods.pyo + " ./setup/exp/LolexToolsSetup.py")
