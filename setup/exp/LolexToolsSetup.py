7da968abe23cf50e615835c8b2aeb1732f9aa5b9
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-13 15:59:08 +0000

    Add header to the users file
    Common bytes? May have to remove possible security issue

--- setup/exp/LolexToolsSetup.py
+++ setup/exp/LolexToolsSetup.py
@@ -14,6 +14,7 @@ except(IOError, OSError):
     pass
 os.mkdir("./TEST")
 users_file = open("./TEST/users.py", "w+")
+users_file.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
 users_file.truncate()
 users = []
 done = "0"
@@ -25,5 +26,5 @@ while done != "1":
     else:
         users.append(user_name)
     done = input("Please enter 1 if you are done adding users.")
-users_file.write("users = " + (str(users)))
+users_file.write("\nusers = " + (str(users)))
 users_file.close()
