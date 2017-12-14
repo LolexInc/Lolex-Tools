9a50e798c210bd7b1ff0acb2e29d7c86754d46c3
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-14 12:59:19 +0000

    Add preliminary main file

---
+++ project/new/main/_main_.py.py
@@ -0,0 +1,19 @@
+#! python3
+##0
+## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+##  0              00     0  0         0           00             00       0      0    0      0   0          0
+##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
+##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+##
+## authors = Monkeyboy2805
+import os, sys, time
+sys.path.insert(0, "./project/old/lib")
+try:
+    import LolexToolsMethods
+except(ImportError):
+    print("Something went wrong here!")
+    time.sleep(5)
+    exit(0)
+print("Nothing available here yet... Entering experimental setup...")
+os.system(LolexToolsMethods.py + "project/new/setup/exp/LolexToolsSetup.py")
