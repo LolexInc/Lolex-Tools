1c3eb08f3bbd75bf07c63aa99e690fec03295c5d
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-15 15:32:08 +0000

    Add welcome message

--- project/new/setup/exp/LolexToolsSetup.py
+++ project/new/setup/exp/LolexToolsSetup.py
@@ -20,6 +20,7 @@ try:
     users_file.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
     users_file.truncate()
     users = []
+    paths = []
     done = "0"
     while done != "1":
         valid = True
@@ -41,8 +42,12 @@ try:
                 folder_string = folder_string + ((str(chr(b))))
         if not os.path.exists("./project/new/TEST/" + (str(folder_string))):
             os.mkdir("./project/new/TEST/" + (str(folder_string)))
+            user_file = open("./project/new/TEST/" + (str(folder_string)) + "Options.py", "w+")
+            user_file.write("print('Welcome " + (str(users[len(users) - 1))) + "')")
+            paths.append(folder_strings)
         done = input("Please enter 1 if you are done adding users.")
     users_file.write("\nusers = " + (str(users)))
+    users_file.write("\npaths = " + (str(paths)))
     users_file.close()
 except(IOError, OSError) as e:
     print(e)
