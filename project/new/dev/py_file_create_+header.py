110fcaed2d6ec7e383dee4bd6078ff29bf4d351c
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-15 14:21:29 +0000

    Update dev extension to add IOError catching

--- project/new/dev/py_file_create_+header.py
+++ project/new/dev/py_file_create_+header.py
@@ -9,7 +9,12 @@
 ## authors = Monkeyboy2805
 import os, time
 path_name = input("Please input your file name")
-with open(path_name + ".py", "a") as outf:
-    outf.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
+try:
+    with open(path_name + ".py", "a") as outf:
+        outf.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
+except(IOError, OSError):
+    print("Failed to create file!")
+    time.sleep(5)
+    exit(0)
 print("Created file")
 time.sleep(5)
