ef1b862f6fbef85fa42ca499718dc4d0ac69b5ca
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-15 15:52:51 +0000

    Sort out new start

--- project/new/start.py
+++ project/new/start.py
@@ -7,19 +7,36 @@
 ##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
 ##
 ## authors = Monkeyboy2805
-import os, sys, time
-sys.path.insert(0, "./project/old/lib")
-try:
+import os, shutil, sys, time
+if sys.version_info.major != 3:
+    print("Please install Python 3 to run this script.")
+    time.sleep(5)
+    exit(0)
+if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
+    IOError = OSError
+sys.path.insert(0, "./project/old/lib/")
+try: 
     import LolexToolsMethods
-except(ImportError) as e:
-    print("ERROR: Something went wrong importing LolexToolsMethods!")
+except(ImportError, SyntaxError, TabError) as e:
     print(e)
+    print("Missing or corrupted library. Please redownload this application or make an issue if this persists.")
+    del sys.path[sys.path.index("./project/old/lib")]
     time.sleep(5)
     exit(0)
-if LolexToolsMethods.uos.useros != "Android" and LolexToolsMethods.uos.useros != "Linux" and LolexToolsMethods.uos.useros != "Windows":
-    print("OS not supported. Currently supported OSs are Linux(including Android) and Windows(newer than Windows ME at minimum, for higher than Python 3.5 you need at least Windows Vista.")
+del sys.path[sys.path.index("./project/old/lib")]
+if LolexToolsMethods.uos.useros == "Windows":
+    os.system("TITLE Lolex-Tools")
+    os.system("MODE 1000")
+elif LolexToolsMethods.uos.useros == "Linux" or LolexToolsMethods.uos.useros == "Android":
+    sys.stdout.write("\x1b]2;Lolex-Tools\x07")
+else:
+    print("OS not supported!!!")
     time.sleep(5)
     exit(0)
-os.system(LolexToolsMethods.pyo + " ./project/new/ext/anim/bootanim.py")
-os.system(LolexToolsMethods.pyo + " ./project/new/main/_main_.py")
-time.sleep(5)
+a = input("Please enter 1 to launch the old project, 0 to launch the new project.")
+if a == "1":
+    os.system(LolexToolsMethods.pyo + " ./project/old/start.py")
+elif a == "0":
+    os.system(LolexToolsMethods.pyo + " ./project/new/start.py")
+else:
+    print("No such recognised version!")
