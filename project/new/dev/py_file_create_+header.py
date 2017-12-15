d1929a62b1603a642c218ac2a192a30d38fa41d6
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-15 14:23:35 +0000

    Add debug output

--- project/new/dev/py_file_create_+header.py
+++ project/new/dev/py_file_create_+header.py
@@ -14,7 +14,8 @@ path_name = input("Please input your file name")
 try:
     with open(path_name + ".py", "a") as outf:
         outf.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
-except(IOError, OSError):
+except(IOError, OSError) as e:
+    print(e)
     print("Failed to create file!")
     time.sleep(5)
     exit(0)
