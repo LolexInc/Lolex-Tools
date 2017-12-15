7f2efd4f7b9d6b449ba015f282d6c8e7ea742200
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-15 14:38:38 +0000

    Stick the main file in a try except just in case you're running in read-only
    But seriously, why would you do that?

--- project/new/setup/exp/LolexToolsSetup.py
+++ project/new/setup/exp/LolexToolsSetup.py
@@ -11,22 +11,26 @@ import time, os, shutil, io, sys
 if sys.version_info.minor > 6 and (sys.version_info[1] == 7 and sys.version_info[2] == 0 and sys.version_info[3] == "alpha" and sys.version[4] == 0) == False:
     IOError = OSError
 try:
-    shutil.rmtree("./project/new/TEST")
+    try:
+        shutil.rmtree("./project/new/TEST")
+    except(IOError, OSError) as e:
+        print(e)
+    os.mkdir("./project/new/TEST")
+    users_file = open("./project/new/TEST/users.py", "w+")
+    users_file.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
+    users_file.truncate()
+    users = []
+    done = "0"
+    while done != "1":
+        valid = True
+        user_name = input("Please enter your desired username.")
+        if user_name in users:
+            print("Username already in use!")
+        else:
+            users.append(user_name)
+        done = input("Please enter 1 if you are done adding users.")
+    users_file.write("\nusers = " + (str(users)))
+    users_file.close()
 except(IOError, OSError) as e:
     print(e)
-os.mkdir("./project/new/TEST")
-users_file = open("./project/new/TEST/users.py", "w+")
-users_file.write("#! python3\n##0\n## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000\n##  0              00     0  0         0           00             00       0      0    0      0   0          0\n##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000\n##    0            00     0  0         0          0  0            00       0      0    0      0   0              0\n##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000\n##\n## authors = Monkeyboy2805")
-users_file.truncate()
-users = []
-done = "0"
-while done != "1":
-    valid = True
-    user_name = input("Please enter your desired username.")
-    if user_name in users:
-        print("Username already in use!")
-    else:
-        users.append(user_name)
-    done = input("Please enter 1 if you are done adding users.")
-users_file.write("\nusers = " + (str(users)))
-users_file.close()
+    print("Something went wrong! Look above for details.")
