fb1ed801efb7fa0e3244fc6913ae652cb3f6b632
Monkeyboy2805 <monkeyboy2805@gmail.com>
2017-12-18 12:44:39 +0000

    Add the same patch to the old start.py

--- project/old/start.py
+++ project/old/start.py
@@ -20,8 +20,16 @@ try:
 except(ImportError, SyntaxError, TabError) as e:
     print(e)
     print("Missing or corrupted library. Please redownload this application or make an issue if this persists.")
+    try:
+        del sys.path[sys.path.index("./project/old/lib")]
+    except(ValueError):
+        pass
     time.sleep(5)
     exit(0)
+try:
+    del sys.path[sys.path.index("./project/old/lib")]
+except(ValueError):
+    pass
 if LolexToolsMethods.uos.useros == "Windows":
     os.system("TITLE Lolex-Tools")
     os.system("MODE 1000")
