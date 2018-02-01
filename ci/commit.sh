c3ff1d7fe12f6830635c85179d93382dc43d6450
Jensen Taylor <monkeyboy2805@gmail.com>
2018-02-01 12:35:27 +0000

    Update commit.sh

--- ci/commit.sh
+++ ci/commit.sh
@@ -1,20 +1,18 @@
-notice "Setting up Git"
-
+echo "\nSetting up Git"
 if [ -z "`git config --get --global credential.helper`" ]; then
-  notice "set up credential.helper"
+  echo "\nset up credential.helper"
   git config credential.helper "store --file=.git/credentials"
-  echo "https://${GITHUB_OAUTH_TOKEN}:@github.com" > .git/credentials 2>/dev/null
+  echo "\nhttps://monkeyboy2805:@github.com" > .git/credentials 2>/dev/null
 fi
 if [ -z "`git config --get --global user.email`" ]; then
-  notice "set up user.email"
+  echo "\nset up user.email"
   git config --global user.email "monkeyboy2805@gmail.com"
 fi
 if [ -z "`git config --get --global user.name`" ]; then
-  notice "set up user.name"
+  echo "\nset up user.name"
   git config --global user.name "Monkeyboy2805"
 fi
-
-notice "Creating commit"
+echo "\nCreating commit"
 git checkout ci_test_bot
 git checkout -b ci_test_bot_AA
 git commit -m "Try to do SOMETHING"
