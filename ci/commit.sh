36713a886fa33ad4263aeac7c9b9a181f826ec18
Jensen Taylor <monkeyboy2805@gmail.com>
2018-02-02 14:21:30 +0000

    Try something
    Doubt it'll work

--- ci/commit.sh
+++ ci/commit.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
-
-setup_git() {
-  git config --global user.email "monkeyboy2805@gmail.com"
-  git config --global user.name "monkeyboy2805"
-}
-
-commit_website_files() {
-  git checkout ci_test_bot
-  git add *
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git push origin ci_test_bot
-}
-
-setup_git
-commit_website_files
-upload_files
+git config --global user.email "monkeyboy2805@gmail.com"
+git config --global user.name "monkeyboy2805"
+git config --global user.token $token
+git checkout ci_test_bot
+git add *
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push origin ci_test_bot
