be47700f859b62a4690788d39bd9639fe3065d10
Jensen Taylor <monkeyboy2805@gmail.com>
2018-02-02 14:15:45 +0000

    Update commit.sh

--- ci/commit.sh
+++ ci/commit.sh
@@ -6,14 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
-  git add . *.html
+  git checkout ci_test_bot
+  git add *
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git push origin ci_test_bot
 }
 
 setup_git
