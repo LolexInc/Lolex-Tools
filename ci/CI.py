077319a57f2bd794a39b408e8bfadb60b407811c
Jensen Taylor <monkeyboy2805@gmail.com>
2018-02-26 12:49:58 +0000

    Sort preliminary changes
    May need reformatting due to Linux text editor

--- ci/CI.py
+++ ci/CI.py
@@ -8,72 +8,76 @@
 ##
 ## authors = Monkeyboy2805
 import os, py_compile, sys
-print("CI version 2.0.1")
-folders = []
-files = []
-root = os.listdir("./")
-arraypos = 0
-while arraypos < len(root):
-	if (".git" in root[arraypos]) == False:
-		readin = True
-		try:
-			r = open(root[arraypos], "r")
-		except(IOError, OSError):
-			readin = False
-		if readin == True:
-			if root[arraypos].endswith(".py"):
-				files.append("./" + root[arraypos])
-		else: pass
-		if readin == False:
-			readin2 = True
-			try:
-				os.listdir(root[arraypos])
-			except(IOError, OSError):
-				readin2 = False
-			if readin2 == True:
-				folders.append("./" + root[arraypos])
-	arraypos = arraypos + 1
-arraypos = 0
-while arraypos<len(folders):
-	path = folders[arraypos] + "/"
-	currsub = os.listdir(path)
-	tarraypos = 0
-	sublen = len(currsub)
-	while tarraypos<sublen:
-		if (".git" in currsub[tarraypos]) == False:
-			readin3 = True
-			try:
-				r = open(path + currsub[tarraypos])
-			except(IOError, OSError):
-				readin3 = False
-			if readin3 == True:
-				if currsub[tarraypos].endswith(".py"):
-					files.append(path + currsub[tarraypos])
-			else: pass
-			if readin3 == False:
-				readin4 = True
-				try:
-					os.listdir(path + currsub[tarraypos])
-				except(IOError, OSError):
-					readin4 = False
-				if readin4 == True:
-					folders.append(path + currsub[tarraypos])
-		tarraypos = tarraypos + 1
-	arraypos = arraypos + 1
-flen = len(files)
-arraypos = 0
-print("Compiling...")
-fail = False
-failers = 0
-while arraypos < flen:
-	currfile = files[arraypos]
-	if type(py_compile.compile(currfile)) is str:
-		print("Successfully to compiled " + (str(currfile)))
-	else:
-		print("Failed to compile " + (str(currfile)))
-		fail = True
-		failers = failers + 1
-	arraypos = arraypos + 1
-if fail == True:
-	print((str(failers)) + " files failed to compile.")
-	exit(1)
+print("CI version 3.0.0 PRERELEASE")
+sys.path.insert(0, "./ci/build/")
+import DJANGO_VERSION as env
+for i in range(0, len(env.versions):
+    if i == len(env.versions):
+        break;
+    os.system("pip install -q django=" + env.versions[i])
+    folders = []
+    files = []
+    root = os.listdir("./")
+    arraypos = 0
+    while arraypos < len(root):
+	    if (".git" in root[arraypos]) == False:
+	        readin = True
+		    try:
+			    r = open(root[arraypos], "r")
+		    except(IOError, OSError):
+			    readin = False
+		    if readin == True:
+			    if root[arraypos].endswith(".py"):
+				    files.append("./" + root[arraypos])
+		    if readin == False:
+			    readin2 = True
+			    try:
+				    os.listdir(root[arraypos])
+			    except(IOError, OSError):
+				    readin2 = False
+			    if readin2 == True:
+				    folders.append("./" + root[arraypos])
+	    arraypos = arraypos + 1
+    arraypos = 0
+    while arraypos<len(folders):
+	    path = folders[arraypos] + "/"
+	    currsub = os.listdir(path)
+	    tarraypos = 0
+	    sublen = len(currsub)
+	    while tarraypos<sublen:
+		    if (".git" in currsub[tarraypos]) == False:
+			    readin3 = True
+			    try:
+				    r = open(path + currsub[tarraypos])
+			    except(IOError, OSError):
+				    readin3 = False
+			    if readin3 == True:
+				    if currsub[tarraypos].endswith(".py"):
+					    files.append(path + currsub[tarraypos])
+			    if readin3 == False:
+				    readin4 = True
+				    try:
+					    os.listdir(path + currsub[tarraypos])
+				    except(IOError, OSError):
+					    readin4 = False
+				    if readin4 == True:
+					    folders.append(path + currsub[tarraypos])
+		    tarraypos = tarraypos + 1
+	    arraypos = arraypos + 1
+    flen = len(files)
+    arraypos = 0
+    print("Compiling...")
+    fail = False
+    failers = 0
+    while arraypos < flen:
+	    currfile = files[arraypos]
+	    if type(py_compile.compile(currfile)) is str:
+		    print("Successfully to compiled " + (str(currfile)))
+	    else:
+		    print("Failed to compile " + (str(currfile)))
+		    fail = True
+		    failers = failers + 1
+	    arraypos = arraypos + 1
+    if fail == True:
+	    print((str(failers)) + " files failed to compile.")
+	    exit(1)
