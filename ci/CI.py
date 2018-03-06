de1faa4f797542cd063621615e438c32ae5708d9
Jensen Taylor <monkeyboy2805@gmail.com>
2018-03-06 09:26:43 +0000

    Fix misuse of currfile in CI.py

--- ci/CI.py
+++ ci/CI.py
@@ -8,24 +8,28 @@
 ##
 ## authors = Monkeyboy2805
 import os, py_compile, sys, time
-version = (str(sys.version_info[0])) + (str(sys.version_info[1])) + (str(sys.version_info[2])) + (str(sys.version_info[4]))
+
+version = (str(sys.version_info[0])) + (str(sys.version_info[1])) + (str(sys.version_info[2])) + (
+str(sys.version_info[4]))
 a = time.time()
 print("CI version 3.0.0 PRERELEASE")
 sys.path.insert(0, "./ci/build/")
 import PYTHON_VERSION_FOR_AUTO as PY_VER
+
 del sys.path[sys.path.index("./ci/build/")]
 sys.path.insert(0, "../")
 import PYTHON_VERSION_FOR_AUTO as PY_VER_OLD
+
 if PY_VER.version < int(version) and sys.version_info[3] == "final":
-        os.remove("./ci/build/PYTHON_VERSION_FOR_AUTO.py")
-        with open("./ci/build/PYTHON_VERSION_FOR_AUTO.py", "a") as outf:
-                outf.write("version = " + str(version))
-                print("Found version was bigger than expected")
-        os.system("git add *")
-        os.system("git commit -am '[ci skip] Update python version in ci/build to " + str(version) + "'")
-        if os.system("git pull --rebase") != 0:
-            exit(127)
-        os.system("git push")
+    os.remove("./ci/build/PYTHON_VERSION_FOR_AUTO.py")
+    with open("./ci/build/PYTHON_VERSION_FOR_AUTO.py", "a") as outf:
+        outf.write("version = " + str(version))
+        print("Found version was bigger than expected")
+    os.system("git add *")
+    os.system("git commit -am '[ci skip] Update python version in ci/build to " + str(version) + "'")
+    if os.system("git pull --rebase") != 0:
+        exit(127)
+    os.system("git push")
 failers = 0
 b = time.time()
 print("Testing...")
@@ -42,7 +46,7 @@ while arraypos < len(root):
             readin = False
         if readin == True:
             if root[arraypos].endswith(".py"):
-                    files.append("./" + root[arraypos])
+                files.append("./" + root[arraypos])
         if readin == False:
             readin2 = True
             try:
@@ -92,7 +96,7 @@ while arraypos < flen:
         failers = failers + 1
     arraypos = arraypos + 1
 if int(version) == int(PY_VER_OLD.version):
-	### Getting RIGHT contents but somehow writing to the wrong files
+    ### Getting RIGHT contents but somehow writing to the wrong files
     print("Updating headers...")
     for j in range(0, len(files) - 1):
         file_opened = open(files[j], "r+")
@@ -100,18 +104,15 @@ if int(version) == int(PY_VER_OLD.version):
         if file_opened_lines[0] != "#! python3":
             file_opened_lines.insert(0, "#! python3\n")
             file_opened.close()
-            try:
-                os.remove(currfile)
-            except(IOError, OSError):
-                pass
-            with open(currfile, "a") as outf:
+            os.remove(files[j])
+            with open(files[j], "a") as outf:
                 for i in range(0, len(file_opened_lines)):
                     if i == len(file_opened_lines):
                         break;
                     outf.write(file_opened_lines[i])
             os.system("git add *")
             os.system("git commit -am '[ci skip] Update shebang line in " + currfile + "'")
-            #os.system("git branch $TRAVIS_BUILD_NUMBER AUTOMATION")
+            # os.system("git branch $TRAVIS_BUILD_NUMBER AUTOMATION")
             if os.system("git pull --rebase") != 0:
                 os.system(127)
             os.system("git push")
