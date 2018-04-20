c2c6017f37eadb1c580a544bb4c6f879efd14f48
Jensen Taylor <monkeyboy2805@gmail.com>
2018-04-20 14:53:36 +0100

    [ci skip] Add file for triggering CI repo auto updating
    Hence the file name

---
+++ ci/build/update/CIRepoAutoUpdate.py
@@ -0,0 +1,14 @@
+#! python3
+##0
+## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+##  0              00     0  0         0           00             00       0      0    0      0   0          0
+##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
+##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+##
+## authors = Monkeyboy2805
+import sys
+sys.path.insert(0, "./ci/lib/")
+import LolexToolsCIlib
+LolexToolsCIlib.update_py_ver()
+LolexToolsCIlib.update_headers()
