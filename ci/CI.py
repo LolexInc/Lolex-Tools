3a4385038bf1ef65a4669ce57b2a364da6d3e853
Jensen Taylor <monkeyboy2805@gmail.com>
2018-04-26 08:29:19 +0000

    Add call to function in the CI lib for getting file folders in test dir

--- ci/CI.py
+++ ci/CI.py
@@ -13,76 +13,12 @@ import sys
 import time
 a = time.time()
 print("CI version 3.0.0 PRERELEASE")
+sys.path.insert(0, "./ci/lib/")
+import LolexToolsCIlib
+del sys.path[sys.path.index("./ci/lib/")]
 b = time.time()
 print("Testing...")
-def get_file_folders():
-    failers = 0
-    folders = []
-    files = []
-    root = os.listdir("./")
-    arraypos = 0
-    while arraypos < len(root):
-        if (".git" in root[arraypos]) == False:
-            readin = True
-            try:
-                r = open(root[arraypos], "r")
-            except(IOError, OSError):
-                readin = False
-            if readin == True:
-                if root[arraypos].endswith(".py"):
-                    files.append("./" + root[arraypos])
-            if readin == False:
-                readin2 = True
-                try:
-                    os.listdir(root[arraypos])
-                except(IOError, OSError):
-                    readin2 = False
-                if readin2 == True:
-                    folders.append("./" + root[arraypos])
-        arraypos = arraypos + 1
-    arraypos = 0
-    while arraypos < len(folders):
-        path = folders[arraypos] + "/"
-        currsub = os.listdir(path)
-        tarraypos = 0
-        sublen = len(currsub)
-        while tarraypos < sublen:
-            if (".git" in currsub[tarraypos]) == False:
-                readin3 = True
-                try:
-                    r = open(path + currsub[tarraypos])
-                except(IOError, OSError):
-                    readin3 = False
-                if readin3 == True:
-                    if currsub[tarraypos].endswith(".py"):
-                        files.append(path + currsub[tarraypos])
-                if readin3 == False:
-                    readin4 = True
-                    try:
-                        os.listdir(path + currsub[tarraypos])
-                    except(IOError, OSError):
-                        readin4 = False
-                    if readin4 == True:
-                        folders.append(path + currsub[tarraypos])
-            tarraypos = tarraypos + 1
-        arraypos = arraypos + 1
-    flen = len(files)
-    arraypos = 0
-    print("Compiling...")
-    fail = False
-    while arraypos < flen:
-        currfile = files[arraypos]
-        if type(py_compile.compile(currfile)) is str:
-            print("Successfully compiled " + (str(currfile)))
-        else:
-            print("Failed to compile " + (str(currfile)))
-            fail = True
-            failers = failers + 1
-        arraypos = arraypos + 1
-    if fail == True:
-        print(str(failers) + " files failed to compile")
-        exit(1)
-get_file_folders()
+LolexToolsCIlib.get_file_folders()
 c = (str(round(time.time() - b, 0)))
 c.replace("-", "")
 print("Tests complete in " + c + " seconds")
