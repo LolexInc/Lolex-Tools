1a9e18cb79f35b4b32c6351d987be84b161683cb
Jensen Taylor <monkeyboy2805@gmail.com>
2018-06-11 15:33:11 +0100

    [ci skip] Fix some more bad identifiers

--- ci/LolexUK.py
+++ ci/LolexUK.py
@@ -1,11 +1,11 @@
 import time, os
 a = time.asctime(time.localtime(time.time())).split()
 if a[1] == "Aug" and a[2] == "1":
-	os.remove(“./README.md”)
-	os.rename(“./README.md.new”, “./README.md”)
-	os.remove(“./.travis.yml”)
-	os.rename(“./.travis.yml.old”, “./travis.yml”)
-	os.remove(“./ci/LolexUK.py”)
+	os.remove("./README.md")
+	os.rename("./README.md.new", "./README.md")
+	os.remove("./.travis.yml")
+	os.rename("./.travis.yml.old", "./travis.yml")
+	os.remove("./ci/LolexUK.py")
 	os.system("git add *")
 	os.system("git commit -am 'Official update to the LolexUK name'")
 	os.system("git push")
