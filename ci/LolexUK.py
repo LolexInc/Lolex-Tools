6e37f437d7bafc4a2ae20152a11490224d823551
Jensen Taylor <monkeyboy2805@gmail.com>
2018-06-11 15:51:31 +0100

    [ci skip] Update LolexUK.py

--- ci/LolexUK.py
+++ ci/LolexUK.py
@@ -4,7 +4,7 @@ if a[1] == "Aug" and a[2] == "1":
 	os.remove("./README.md")
 	os.rename("./README.md.new", "./README.md")
 	os.remove("./.travis.yml")
-	os.rename("./travis.yml.old", "./travis.yml")
+	os.rename("./travis.yml.old", "./.travis.yml")
 	os.remove("./ci/LolexUK.py")
 	os.system("git add *")
 	os.system("git commit -am 'Official update to the LolexUK name'")
