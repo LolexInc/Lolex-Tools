84c3fddf1c0139ae659278ff30dbefb82de6bea4
monkeyboy2805 <monkeyboy2805@gmail.com>
2018-08-09 11:50:41 +0100

    Remove old x86 and x64 files and replace them with new files

--- install/Windows/x86/download.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /P proxy=Press enter to continue if you are NOT using a proxy. Otherwise refer to README.txt 
-.\curl\curl --url "https://www.python.org/ftp/python/3.7.0/python-3.7.0-embed-win32.zip" --location --output "./python.zip" --insecure
-.\curl\curl --url "https://github.com/LolexUK/Lolex-Tools/releases/download/vcruntime140.dll_download/vcruntime140.dll" --location --output "./vcruntime140.dll" --insecure
