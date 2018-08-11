7bf88e2a4624c7734ed1fee2194035764fbd0918
monkeyboy2805 <monkeyboy2805@gmail.com>
2018-08-11 10:51:37 +0100

    Fix incorrect paths in download.py

--- install/Windows/x86/download.py
+++ install/Windows/x86/download.py
@@ -3,4 +3,4 @@ proxy = input("Enter Y if you are using a proxy or enter anything else if you ar
 if proxy == "Y":
 	os.system(".\curl\curl -x " + input("Enter your proxy in the format proxy_address:port") + " --url https://github.com/lolexuk/lolex-tools/zipball/latest --location --insecure --output ./package.zip")
 else:
-	os.system(".\curl\curl --url https://github.com/lolexuk/lolex-tools/zipball/latest --location --insecure --output D:/package.zip")
+	os.system(".\curl\curl --url https://github.com/lolexuk/lolex-tools/zipball/latest --location --insecure --output ./package.zip")
