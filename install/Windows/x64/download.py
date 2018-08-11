16c9b1e5aeb55359fb69adca2540253fed5805ce
monkeyboy2805 <monkeyboy2805@gmail.com>
2018-08-11 11:18:20 +0100

    Add extraction of the project once curl has finished downloading

--- install/Windows/x64/download.py
+++ install/Windows/x64/download.py
@@ -1,6 +1,13 @@
-import os, time
+import os, time, zipfile
 proxy = input("Enter Y if you are using a proxy or enter anything else if you aren't or you don't know what this means. ")
 if proxy == "Y":
 	os.system(".\curl\curl -x " + input("Enter your proxy in the format proxy_address:port") + " --url https://github.com/lolexuk/lolex-tools/zipball/latest --location --insecure --output ./package.zip")
 else:
 	os.system(".\curl\curl --url https://github.com/lolexuk/lolex-tools/zipball/latest --location --insecure --output ./package.zip")
+zip_ref = zipfile.ZipFile("./package.zip", "r")
+zip_ref.extractall("./")
+zip_ref.close()
+a = os.listdir("./")
+for i in range(0, len(a) - 1):
+	if "Lolex-Tools" in a[i]:
+		os.rename("./" + a[i], "./Lolex-Tools")
