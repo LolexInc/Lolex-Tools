901d2f4f325c9c098ee370ae524fc425ff4154fc
monkeyboy2805 <monkeyboy2805@gmail.com>
2019-08-08 16:39:06 +0100

    Remove pointless shutil import and separate useful imports out onto separate lines in base start.py

--- start.py
+++ start.py
@@ -7,7 +7,9 @@
 #      0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
 #
 # authors = Monkeyboy2805
-import os, shutil, sys, time
+import os
+import sys
+import time
 if sys.version_info.major != 3:
 	print("Please install Python 3 to run this script.")
 	time.sleep(5)
