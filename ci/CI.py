496c3f690a4b1297a4dcf0b3ffe99ac1afcaf79c
monkeyboy2805 <monkeyboy2805@gmail.com>
2019-08-08 16:34:03 +0100

    Pycharm reformatting CI.py

--- ci/CI.py
+++ ci/CI.py
@@ -1,12 +1,12 @@
 #! python3
-##0
-## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
-##  0              00     0  0         0           00             00       0      0    0      0   0          0
-##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
-##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
-##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
-##
-## authors = Monkeyboy2805
+# 0
+#  0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+#   0              00     0  0         0           00             00       0      0    0      0   0          0
+#    0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+#     0            00     0  0         0          0  0            00       0      0    0      0   0              0
+#      0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+#
+# authors = Monkeyboy2805
 import os
 import py_compile
 import sys
@@ -19,6 +19,7 @@ del sys.path[sys.path.index("./ci/lib/")]
 b = time.time()
 print("Testing...")
 LolexToolsCIlib.compile_files(LolexToolsCIlib.get_file_folders())
+LolexToolsCIlib.update_py_ver()
 c = (str(round(time.time() - b, 0)))
 c.replace("-", "")
 print("Tests complete in " + c + " seconds")
