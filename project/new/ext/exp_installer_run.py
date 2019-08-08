83ac66ade5348fe0f5fcb7c0f9a2497a45c20685
monkeyboy2805 <monkeyboy2805@gmail.com>
2019-08-08 15:57:39 +0100

    Separate imports out onto separate lines in new installer

--- project/new/ext/exp_installer_run.py
+++ project/new/ext/exp_installer_run.py
@@ -1,5 +1,6 @@
 #! python3
-import os, sys
+import os
+import sys
 if input("Do you want to enter the experimental installer? Please enter 1 if so. (DEVELOPERS TESTING ONLY!!!") == "1":
     sys.path.insert(0, "./lib")
     import LolexToolsMethods
