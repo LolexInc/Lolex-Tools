18e41b3faf68aea529ec7da3b70a35f19126f896
monkeyboy2805 <monkeyboy2805@gmail.com>
2019-08-09 11:02:30 +0100

    Pycharm reformatting new project LolexToolsMethods.py

--- project/new/lib/LolexToolsMethods.py
+++ project/new/lib/LolexToolsMethods.py
@@ -1,29 +1,34 @@
 #! python3
-##0
-## 0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
-##  0              00     0  0         0           00             00       0      0    0      0   0          0
-##   0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
-##    0            00     0  0         0          0  0            00       0      0    0      0   0              0
-##     0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
-##
-## authors = Monkeyboy2805
+# 0
+#  0                000000   0         000000     0  0         000000000   00000000    00000000   0          000000
+#   0              00     0  0         0           00             00       0      0    0      0   0          0
+#    0             00     0  0         00000       00   000000    00       0      0    0      0   0          00000
+#     0            00     0  0         0          0  0            00       0      0    0      0   0              0
+#      0000000      000000   0000000   000000    0    0           00       00000000    00000000   0000000   000000
+#
+# authors = Monkeyboy2805
 import sys
+
+
 class add_user:
-    def setup_folder(self):
-        pass
-    def add_user_to_file(self):
-        pass
+	def setup_folder(self):
+		pass
+
+	def add_user_to_file(self):
+		pass
+
+
 class authenticate:
-    @staticmethod
-    def login():
-        try:
-            sys.path.insert(0, "./project/new/setup/exp/TEST")
-            import users
-            usernameenter = input("Please enter your username.")
-            if usernameenter in users.users:
-                path = users.paths[users.users.index(usernameenter)]
-                print("Welcome " + (str(usernameenter)))
-            else:
-                return 1
-        except(ImportError):
-            return 2
+	@staticmethod
+	def login():
+		try:
+			sys.path.insert(0, "./project/new/setup/exp/TEST")
+			import users
+			usernameenter = input("Please enter your username.")
+			if usernameenter in users.users:
+				path = users.paths[users.users.index(usernameenter)]
+				print("Welcome " + (str(usernameenter)))
+			else:
+				return 1
+		except ImportError:
+			return 2
